55c8248cdde727953b2a80dfbdc2657b0c06599a
Thomas Dean <tommydean_sd@yahoo.com>
2017-08-12 16:43:25 -0700

    Wrap getword.h in header guard

--- getword.h
+++ getword.h
@@ -10,6 +10,9 @@
   for the simple things your initial attempt a creating getword.c should handle.
 */
 
+#ifndef _GETWORD_H
+#define _GETWORD_H
+
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
@@ -17,3 +20,4 @@
 #define STORAGE 255
 
 int getword(char *w);
+#endif
