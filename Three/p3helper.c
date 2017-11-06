62896713e1138d642cfe3e421d9dfbb8cc3beeff
Thomas Dean <tommydean_sd@yahoo.com>
2017-11-06 12:28:08 -0800

    Outline different cases based on protocol and access kind

--- Three/p3helper.c
+++ Three/p3helper.c
@@ -9,7 +9,7 @@
  */
 #include "p3.h"
 
-/* 
+/*
  * General documentation for the following functions is in p3.h
  */
 void initstudentstuff(int protocol)
@@ -18,8 +18,46 @@ void initstudentstuff(int protocol)
 
 void prolog(int kind, int protocol)
 {
+    if (kind == READER) {
+        if (protocol == FAIR) {
+            /* Do fair reader prolog */
+        } else if (protocol == WRIT) {
+            /* Do writer biased prolog for reader */
+        } else {
+            fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
+        }
+    } else if (kind = WRITER) {
+        if (protocol == FAIR) {
+            /* Do fair writer prolog */
+        } else if (protocol == WRIT) {
+            /* Do writer biased prolog for writer */
+        } else {
+            fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
+        }
+    } else {
+        fprintf(stderr, "Unknown access type, expected READER or WRITER\n");
+    }
 }
 
 void epilog(int kind, int protocol)
 {
+    if (kind == READER) {
+        if (protocol == FAIR) {
+            /* Do fair reader epilog */
+        } else if (protocol == WRIT) {
+            /* Do writer biased epilog for reader */
+        } else {
+            fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
+        }
+    } else if (kind = WRITER) {
+        if (protocol == FAIR) {
+            /* Do fair writer epilog */
+        } else if (protocol == WRIT) {
+            /* Do writer biased epilog for writer */
+        } else {
+            fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
+        }
+    } else {
+        fprintf(stderr, "Unknown access type, expected READER or WRITER\n");
+    }
 }
