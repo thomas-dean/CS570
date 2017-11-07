890b1e6056b29468c816cf3087d8ece49241ae9f
Thomas Dean <tommydean_sd@yahoo.com>
2017-11-07 02:46:14 -0800

    Implement writer biased protocol
    Also fixed dumb `==` typo in if statement

--- Three/p3helper.c
+++ Three/p3helper.c
@@ -9,11 +9,24 @@
  */
 #include "p3.h"
 
+static int rcount, wcount;
+static sem_t wlock, rlock, wcountlock, rcountlock, renterlock;
+
 /*
  * General documentation for the following functions is in p3.h
  */
 void initstudentstuff(int protocol)
 {
+    if (protocol == FAIR) {
+    } else if (protocol == WRIT) {
+        pCHK(sem_init(&wlock, 0, 1));
+        pCHK(sem_init(&rlock, 0, 1));
+        pCHK(sem_init(&wcountlock, 0, 1));
+        pCHK(sem_init(&rcountlock, 0, 1));
+        pCHK(sem_init(&renterlock, 0, 1));
+    } else {
+        fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
+    }
 }
 
 void prolog(int kind, int protocol)
@@ -23,14 +36,33 @@ void prolog(int kind, int protocol)
             /* Do fair reader prolog */
         } else if (protocol == WRIT) {
             /* Do writer biased prolog for reader */
+            pCHK(sem_wait(&renterlock));
+            pCHK(sem_wait(&rlock));
+            pCHK(sem_wait(&rcountlock));
+            if (++rcount == 1) {
+                /* I am the first reader to come along, let's wait for the
+                 * writers to clear up... */
+                pCHK(sem_wait(&wlock));
+            }
+            pCHK(sem_post(&rcountlock));
+            pCHK(sem_post(&rlock));
+            pCHK(sem_post(&renterlock));
         } else {
             fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
         }
-    } else if (kind = WRITER) {
+    } else if (kind == WRITER) {
         if (protocol == FAIR) {
             /* Do fair writer prolog */
         } else if (protocol == WRIT) {
             /* Do writer biased prolog for writer */
+            pCHK(sem_wait(&wcountlock));
+            if (++wcount == 1) {
+                /* I am the first writer to come along, let's wait for the
+                 * remaining readers to finish */
+                pCHK(sem_wait(&rlock));
+            }
+            pCHK(sem_post(&wcountlock));
+            pCHK(sem_wait(&wlock));
         } else {
             fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
         }
@@ -46,14 +78,27 @@ void epilog(int kind, int protocol)
             /* Do fair reader epilog */
         } else if (protocol == WRIT) {
             /* Do writer biased epilog for reader */
+            pCHK(sem_wait(&rcountlock));
+            if (--rcount == 0) {
+                /* I was the last reader to leave, let's allow a writer to enter */
+                pCHK(sem_post(&wlock));
+            }
+            pCHK(sem_post(&rcountlock));
         } else {
             fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
         }
-    } else if (kind = WRITER) {
+    } else if (kind == WRITER) {
         if (protocol == FAIR) {
             /* Do fair writer epilog */
         } else if (protocol == WRIT) {
             /* Do writer biased epilog for writer */
+            pCHK(sem_post(&wlock));
+            pCHK(sem_wait(&wcountlock));
+            if (--wcount == 0) {
+                /* I was the last writer to leave, let's let the a reader enter */
+                pCHK(sem_post(&rlock));
+            }
+            pCHK(sem_post(&wcountlock));
         } else {
             fprintf(stderr, "Unknown protocol type, expected FAIR or WRIT\n");
         }
